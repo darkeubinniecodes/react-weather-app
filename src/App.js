33c1b7c5d086283bd224775515c24d92228b2e87
Catarina Santos <catarinasantos.info@gmail.com>
2024-01-02 15:57:44 +0000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Catarina Santos and it's{" "}
+        <a
+          href="https://github.com/darkeubinniecodes/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
