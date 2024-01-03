d1846044d14aed0ace561e39915093eb6c9749ae
Catarina Santos <catarinasantos.info@gmail.com>
2024-01-03 15:12:47 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Catarina Santos and it's{" "}
-        <a href="https://github.com/darkeubinniecodes/react-weather-app">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Catarina Santos and it's{" "}
+          <a href="https://github.com/darkeubinniecodes/react-weather-app">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
